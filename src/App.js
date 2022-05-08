a27a347581dff27df0bb3056a5ba8cd90e129f9c
miriacode <miriamacunaenciso@gmail.com>
2022-05-08 15:10:11 -0500

    Bonus: Sensei

--- src/App.js
+++ src/App.js
@@ -3,28 +3,68 @@ import './App.css';
 import ToDoForm from './components/ToDoForm';
 import ToDoTable from './components/ToDoTable';
 
+//Bonus Sensei
+// import ToDoStorage from './components/storage/ToDoStorage';
+const ToDoStorage = require('./components/storage/ToDoStorage')
+
 function App() {
 
-  const [db, setDb] = useState([]);
+  //Bonus Sensei
+  const toDoStorage = new ToDoStorage("key");
+  // console.log(n)
+
+  const [db, setDb] = useState(toDoStorage.getTasks());
   const [idCounter, setIdCounter] = useState(1);
+
+  
 
   const createTask = (task) => {
     task.id = idCounter;
     setDb([...db,task]);
     console.log(task)
     setIdCounter(idCounter+1);
+    //Bonus Sensei:
+    toDoStorage.saveTasks([...db,task])    
   }
 
   const checkTask = (id) =>{
     db.map(task=>task.id===id? (task.isDone = !(task.isDone)):(null));
     setDb(db);
     console.log(db)
+
+    //Bonus Sensei:
+    toDoStorage.saveTasks(db)  
   }
 
   const deleteTask = (id) =>{
     let newDb = db.filter((task) => task.id !== id);
     setDb(newDb);
+
+    //Bonus Sensei:
+    toDoStorage.saveTasks(newDb)  
   }
+
+  
+
+  // saveTasks(tasks,key) {
+  //   let myStorage = window.localStorage;
+  //   myStorage.removeItem(key);
+  //   const stringifiedTasks = JSON.stringify(tasks);
+  //   myStorage.setItem(key, stringifiedTasks);
+  // }
+
+  // getTasks(tasks,key) {
+  
+  //   let myStorage = window.localStorage;
+  //   const stringifiedTasks = myStorage.getItem(key);
+  //   const parsedTasks = JSON.parse(stringifiedTasks);
+    
+  //   if(parsedTasks && parsedTasks.length){
+  //     return parsedTasks.map(Task.copyFrom);
+  //   }else{
+  //     return new Array()
+  //   }
+  // }
 
   return (
     <div className="App">
---
+++ src/components/storage/ToDoStorage.js
@@ -0,0 +1,25 @@
+class ToDoStorage {
+    constructor(key) {
+      this.myStorage = window.localStorage;
+      this.key = key;
+    }
+  
+    saveTasks(tasks) {
+      this.myStorage.removeItem(this.key);
+      const stringifiedTasks = JSON.stringify(tasks);
+      this.myStorage.setItem(this.key, stringifiedTasks);
+    }
+  
+    getTasks() {
+      const stringifiedTasks = this.myStorage.getItem(this.key);
+      const parsedTasks = JSON.parse(stringifiedTasks);
+      
+      if(parsedTasks && parsedTasks.length){
+        return parsedTasks;
+      }else{
+        return [];
+      }
+    }
+  }
+
+  module.exports = ToDoStorage;
